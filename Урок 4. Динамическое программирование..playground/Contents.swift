9cf712f73996af51d7f10818761b864fb70c1e83
Tublad <tublad92@gmail.com>
2020-03-31 11:43:30 +0600

    correctly code for homeWork lesson 4

--- Урок 4. Динамическое программирование..playground/Contents.swift
+++ Урок 4. Динамическое программирование..playground/Contents.swift
@@ -6,6 +6,7 @@ import Foundation
 
 // Это сейчас без препятсвий
 
+
 func printSearch(one: Int, two: Int, array: [[Int]]) {
     for i in 0..<one {
         for j in 0..<two {
@@ -38,10 +39,39 @@ func getMainSearch(countOne N: Int, countTwo M: Int) {
 
 getMainSearch(countOne: 3, countTwo: 3)
 
-//2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
+// Вроде бы получилось сделать с препятсвием
+// единтсвенное у меня карта с препятсвиями статична
+var map: Array = [[0,0,0],[0,1,0],[0,1,0]]
 
-var i: Int = 0, j = 0, m = 0, n = 0
-var c: Array = Array(repeating: Array(repeating: 0, count: 8), count: 8)
-var x: Array = Array(repeating: 0, count: 8), y = Array(repeating: 0, count: 8)
-var b: Array = Array(repeating: Array(repeating: 0, count: 8), count: 8)
+func getWithSearch(countOne N: Int, countTwo M: Int) {
+    
+    var A = Array(repeating: Array(repeating: 0, count: N), count: M)
+    print(A)
+    print("вот я сейчас начну делать")
+    for j in 0..<M {
+        if map[0][j] != 1 {
+            A[0][j] = 1
+            print(A[0][j])
+        }
+        print("Походу я вылетаю на первом цикле)) А теперь не вылетает и работает")
+        for i in 1..<N {
+            if map[i][0] != 1 {
+                A[i][0] = 1
+                print("Тут я кое что сделал и цикл прошел дальше, корректно с работая ")
+                print(A[i][0])
+            }
+            for j in 1..<M {
+                if map[i][j] != 1 {
+                    A[i][j] = A[i][j - 1] + A[i - 1][j]
+                }
+            }
+            printSearch(one: N, two: M, array: A)
+        }
+    }
+}
+
+getWithSearch(countOne: 3, countTwo: 3)
+
+
+//2. Решить задачу о нахождении длины максимальной последовательности с помощью матрицы.
 
